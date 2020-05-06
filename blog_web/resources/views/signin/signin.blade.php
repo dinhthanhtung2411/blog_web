c9fa0814cab34cfcec1915913a1a79af155239fa
dinhthanhtung2411 <dinhthanhtung2411@gmail.com>
2020-05-06 14:59:17 +0700

    validate login

--- blog_web/resources/views/signin/signin.blade.php
+++ blog_web/resources/views/signin/signin.blade.php
@@ -39,14 +39,14 @@
 					</span>
 
 
-                <div class="wrap-input100 validate-input" data-validate = "Email/Passord sai">
+                <div class="wrap-input100 validate-input" data-validate = "Email/Password sai">
                     <input class="input100" type="email" name="email" id="email" value="{{old('email')}}">
                     <span class="focus-input100"></span>
                     <span class="label-input100">Email</span>
                 </div>
 
 
-                <div class="wrap-input100 validate-input" data-validate="Email/Passord sai">
+                <div class="wrap-input100 validate-input" data-validate="Email/Password sai">
                     <input class="input100" type="password" name="password">
                     <span class="focus-input100"></span>
                     <span class="label-input100">Password</span>
