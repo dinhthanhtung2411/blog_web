50ac97efef6815dcb3f3ef1a15aac256ec195e6c
dinhthanhtung2411 <dinhthanhtung2411@gmail.com>
2020-05-21 19:21:28 +0700

    .

---
+++ blog_web/app/Http/Controllers/gg.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\RequsetFormSignin;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class gg extends Controller
+{
+    public function index(){
+        return view('signin.signin');
+    }
+
+    public function signin(RequsetFormSignin $request)
+    {
+        $data = [
+            'email' => $request->email,
+            'password' => $request->password
+        ];
+        $remember = $request->remember;
+
+        if (Auth::attempt($data, $remember)){
+            if (Auth::user()->role == RoleConstant::ADMIN) {
+                return redirect('/admin');
+            }
+            return redirect('/');
+        }
+        return back()->with('\'wrong\', "Wrong password! Try again!"');
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/signin');
+    }
+}
